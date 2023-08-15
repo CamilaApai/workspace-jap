fb34758a4c9fe7fa58c05e0dadefb2f7be84094c
justina6700 <agustinamicaelagarcia@gmail.com>
2023-08-15 12:27:02 -0300

    ahora es necesario poner algo en usuario y contra

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let boton = document.getElementById("botoncito");
     
     boton.addEventListener("click", function() {
+       if (inputContraseña.value && inputUsuario.value) != "" 
         window.location.href = "index.html";
    
     });
