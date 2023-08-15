7327f45998bb0c12d37097ea7a14ea730aac6be4
justina6700 <agustinamicaelagarcia@gmail.com>
2023-08-15 10:55:59 -0300

    actividad 3 hecha

---
+++ js/login.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let boton = document.getElementById("botoncito");
+    
+    boton.addEventListener("click", function() {
+        window.location.href = "index.html";
+   
+    });
+    
+   
+});
