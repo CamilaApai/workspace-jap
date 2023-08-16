a17300967f5bf6f7e498769b5d39a1ac3bbdabdb
justina6700 <agustinamicaelagarcia@gmail.com>
2023-08-15 21:24:43 -0300

    terminado

--- js/login.js
+++ js/login.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     let boton = document.getElementById("botoncito");
     let inputUsuario = document.getElementById("inputUsuario"); 
     let inputContraseña = document.getElementById("inputContraseña");
     
-    boton.addEventListener("click", function() {
+    boton.addEventListener("click", function(event) {
+        event.preventDefault();
        if ((inputUsuario.value !== "") && (inputContraseña.value !== ""))
        
     { window.location.href = "index.html";} 
