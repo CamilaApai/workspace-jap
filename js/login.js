02f32f681b30d860881c1c93220a8e08590552a2
IgnacioMari13 <137411722+ignaciomari@users.noreply.github.com>
2023-08-18 18:54:25 -0300

    Comentarios login-js

--- js/login.js
+++ js/login.js
@@ -1,15 +1,23 @@
+// Espera a que el DOM esté completamente cargado antes de ejecutar el código.
 document.addEventListener("DOMContentLoaded", function() {
+    // Obtiene la referencia al elemento del botón mediante su ID.
     let boton = document.getElementById("botoncito");
+    // Obtiene la referencia al campo de entrada del usuario mediante su ID.
     let inputUsuario = document.getElementById("inputUsuario"); 
+    // Obtiene la referencia al campo de entrada de la contraseña mediante su ID.
     let inputContraseña = document.getElementById("inputContraseña");
     
+    // Agrega un evento de clic al botón.
     boton.addEventListener("click", function(event) {
+        // Evita que el evento predeterminado del clic del botón se produzca (en este caso, el envío del formulario).
         event.preventDefault();
-        
-        if (inputUsuario.value !== "" && inputContraseña.value !== "") {
-            localStorage.setItem("usuarioInicioSesion", "true");
+
+        // Comprueba si ambos campos, de usuario y contraseña, no están vacíos.
+        if ((inputUsuario.value !== "") && (inputContraseña.value !== "")) {
+            // Si ambos campos están completos, redirige al usuario a la página "index.html".
             window.location.href = "index.html";
         } else {
+            // Si al menos uno de los campos está vacío, muestra una alerta para informar al usuario.
             alert("Necesitas rellenar ambos campos de datos!");
         }
     });
