e21cb8ddaa26f99e4d84eb9996ae012fca71406a
mateo1627 <mateogoni1627@outlook.com>
2023-08-21 18:57:13 -0300

    Edité comentarios de products.js

--- js/products.js
+++ js/products.js
@@ -1,10 +1,9 @@
-//array donde se cargarán los datos recibidos:
+//Inicializa un array vacio donde se cargarán los datos recibidos e inicializa una variable
 let productsArray = [];
-
-//función que recibe un array con los datos, y los muestra en pantalla a través del DOM
+let htmlContentToAppend = "";
+//Función que recibe un array como parametro
 function showProductsList(array){
-    let htmlContentToAppend = "";
-//recorre el array y va agregando la info en distintas "tarjetas" de los autos en htmlContentToAppend, la información que se ve de dichos autos son la foto de cada uno, el precio, tipo de moneda, nombre, descripción y cuántos fueron vendidos.
+//Recorre el array y le concatena la info de los elementos requeridos del array de objetos "products" a htmlContentToAppend por cada valor de i
     for(let i = 0; i < array.length; i++){ 
         let products = array[i];
         htmlContentToAppend += `
@@ -29,7 +28,7 @@ function showProductsList(array){
         </div>
       
         `
-        // busca con el dom el elemento con id cat-list-container y muestra dentro de él lo que contiene htmlContentToAppend (las tarjetas con la info de los autos)
+        //Busca con el dom el elemento con id cat-list-container y muestra dentro de él lo que contiene htmlContentToAppend
         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
     
     }
@@ -38,16 +37,16 @@ function showProductsList(array){
 
 
 
-// Una vez quese carga la página se llama a getJSONData() pasándole por parámetro la constante que contiene la url del json para obtener la lista de autos.
+//Una vez que carga la página se usa getJSONData(autitosURL) para obtener la lista de autos
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(autitosURL).then(function(resultObj){
-      // Se verifica el estado del objeto que devuelve, y si es correcto, se cargan los datos en productsArray.
+      //Se verifica el estado del objeto que devuelve, y si es correcto, se cargan los datos en productsArray
         if (resultObj.status === "ok")
         {
-          //ahora productsArray va a contener la información de los autos
+          //Ahora productsArray va a contener la información de los autos
             productsArray = resultObj.data.products;
-            // se llama a showProductsList() pasándole por parámetro productsArray.
-            showProductsList(productsArray);
+            //Se llama a showProductsList() pasándole por parámetro productsArray
+             showProductsList(productsArray);
         }
     });
 });
