0b735ca06878dda15fd6f77c3ecd01f98803fffd
mateo1627 <mateogoni1627@outlook.com>
2023-08-29 13:10:11 -0300

    Alerta modificada

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let inputUsuario = document.getElementById("inputUsuario"); 
     // Obtiene la referencia al campo de entrada de la contraseña mediante su ID.
     let inputContraseña = document.getElementById("inputContraseña");
-    
+    let alertaMostrada = false;
     // Agrega un evento de clic al botón.
     boton.addEventListener("click", function(event) {
         // Evita que el evento predeterminado del clic del botón se produzca (en este caso, el envío del formulario).
@@ -18,8 +18,13 @@ document.addEventListener("DOMContentLoaded", function() {
             window.location.href = "index.html";
         } else {
             // Si al menos uno de los campos está vacío, muestra una alerta para informar al usuario.
-            alert("Necesitas rellenar ambos campos de datos!");
-        }
+            if(!alertaMostrada) {
+                const pAlerta = document.createElement("p");
+                pAlerta.textContent = "Necesitas rellenar ambos campos de datos!";
+                alertaContainer.appendChild(pAlerta);
+                alertaMostrada = true;
+                }
+            }
         // Verifica si se ha ingresado un valor en el campo de usuario y en el campo de contraseña
         if (inputUsuario.value !== "" && inputContraseña.value !== "") {
         // Si ambos campos tienen valores no vacíos, procede a marcar al usuario como "inició sesión"
