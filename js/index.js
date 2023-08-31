573467679f26eb81722de5637c33f3bc704509a4
Cristhian Morales <cristhianmoralesmendaro@gmail.com>
2023-08-31 09:56:48 -0300

    ARREGLANDO CAGADAS

---
+++ js/index.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded",function(){
+   
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html";
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html";
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html";
+    });
+
+   
+    let usuarioInicioSesion = localStorage.getItem("usuarioInicioSesion");
+    
+    if (!usuarioInicioSesion) {
+        window.location.href = "my-profile.html"; 
+    }
+
+   
+    const enlaceInicioSesion = document.getElementById('inicioSesion').querySelector('a');
+    const usuariovalue = localStorage.getItem('usuariovalue');
+    if (usuariovalue !== null) {
+        enlaceInicioSesion.href = "my-profile.html" 
+        enlaceInicioSesion.textContent = usuariovalue;
+    }
+
+});
